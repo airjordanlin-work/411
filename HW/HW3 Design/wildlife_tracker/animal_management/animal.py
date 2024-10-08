a195ec2e1eed3169f17bccefef5e136d3b53f34b
Airjordanlin-work <jlinwork0605@gmail.com>
2024-10-07 23:03:30 -0400

    Finished importation module

--- HW/HW3 Design/wildlife_tracker/animal_management/animal.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -2,24 +2,18 @@ from typing import Any, List, Optional
 
 
 
-from wildlife_tracker.animal_management.animal import Animal
+
 
 
 class Animal:
-    def __init__(self, animal_id: int, species: str, health_status: Optional[str] = None, age: Optional[int] = None):
+    def __init__(self, animal_id: int, species: str, health_status: Optional[str] = None, age: Optional[int] = None) -> None:
         self.animal_id = animal_id
         self.species = species
         self.health_status = health_status
         self.age = age
 
-    def assign_animals_to_habitat(animals: List[Animal]) -> None:
-        pass
+def assign_animals_to_habitat(self, animals: List[Animal]) -> None:
+    pass
 
-    def register_animal(animal: Animal) -> None:
-        pass
-
-    def remove_animal(self) -> None:
-        pass
-
-    def get_animal_details(self) -> dict[str, Any]:
-        pass
+def get_animal_details(self) -> dict[str, Any]:
+    pass
